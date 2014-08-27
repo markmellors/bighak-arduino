70562083afe82817216357c3e7a6bec08dc30dc6
markmellors <mark.mellors@gmail.com>
2014-08-27 21:18:26 +0100

    made the serial comms more robust and failsafe

--- bighak_arduino.ino
+++ bighak_arduino.ino
@@ -1,13 +1,11 @@
-//#include <PinChangeInt.h> // available at http://www.arduino.cc/playground/Main/PinChangeInt
-
-
-const int FwdRCPin = 5; //radio control input pin 'elevator', used for forward/reverse
-const int SteerRCPin = 6; //radio control input pin, 'aileron', used for steering
+const int LeftRCPin = 5; //radio control input pin 'elevator', used for forward/reverse
+const int RightRCPin = 6; //radio control input pin, 'aileron', used for steering
 const int EnableRCPin = 7; //radio control input pin,'throttle' used to decide when to override the 
 const int LaserRCPin = 4; //radio control input pin,, should be 'rudder'
 const int LeftMotorOutPin = 3; //output to opamp to generate motor controller voltages
 const int RightMotorOutPin =11; //output to opamp to generate motor controller voltages
 const int LaserOutPin = 8; //output to relay to fire laser
+const int RCSwitchPin = 12; //needs comfirming, this is the lien connected to the physical toggle switch for RC
 const int ControllerEnablePin = 2; //output to relay for turning controller on  
 const int MotorNeutral = 133; //pwm level for motors off
 const int MotorMaxFWD = 194;  //185 equivalent to 3.62
@@ -16,25 +14,27 @@ const int RCMax = 1550;  //absolute max: 2003; //max microseconds
 const int RCMin = 1350;  //absalute min: 986; //min microseconds 
 const int RCEnable = 1700; //cut in level
 const int RCDisable = 1200; //cut out level
-// variables to keep track of RC signals. 'rising_times' are used to track when signal pusles started. '_Signal' variables used to store last received pulse length
-unsigned long risingFWDTime = 0;
-unsigned long risingSteeringTime = 0;
-unsigned long risingEnableTime = 0;
-unsigned long risingLaserTime = 0;
-double FWDSignal=1500, SteeringSignal=1500, EnableSignal=1500, LaserSignal=1500;
-int LeftMotorSignal, RightMotorSignal; //used to store desired motor speeds
+boolean LaserOn, NoCommand;
+float LastCommand;
+char Command;
+double LeftSignal=1500, RightSignal=1500, EnableSignal=1500, LaserSignal=1500;
 float LeftMotorSpeed, RightMotorSpeed;
 
+
 void setup() {
-          
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Supposedly Needed for Leonardo only
+  }
   pinMode(LeftMotorOutPin, OUTPUT);
   pinMode(RightMotorOutPin, OUTPUT);
   pinMode(LaserOutPin, OUTPUT);
   pinMode(ControllerEnablePin, OUTPUT);
-  pinMode(FwdRCPin,INPUT);
-  pinMode(SteerRCPin,INPUT);
+  pinMode(LeftRCPin,INPUT);
+  pinMode(RightRCPin,INPUT);
   pinMode(EnableRCPin,INPUT);
   pinMode(LaserRCPin,INPUT);
+  pinMode(RCSwitchPin,INPUT);
   digitalWrite(ControllerEnablePin,LOW);
   analogWrite(LeftMotorOutPin,MotorNeutral);
   analogWrite(RightMotorOutPin,MotorNeutral);
@@ -45,24 +45,88 @@ void setup() {
 
 void loop(){
 // channel fitlering. TODO: check for invalid, or outof range values and do something
-  FWDSignal = pulseIn(FwdRCPin, HIGH, 25000); // Read the pulse width of 
-  SteeringSignal = pulseIn(SteerRCPin, HIGH, 25000); // each channel
-  FWDSignal = min(max(FWDSignal, RCMin), RCMax);
-  SteeringSignal = min(max(SteeringSignal, RCMin), RCMax);
+  EnableSignal = pulseIn(EnableRCPin, HIGH, 25000);
+  if (EnableSignal > 1500){
+    if (digitalRead(RCSwitchPin)){
+      LeftSignal = pulseIn(LeftRCPin, HIGH, 25000); // Read the pulse width of 
+      RightSignal = pulseIn(RightRCPin, HIGH, 25000); // each channel
+      // constrain signals
+    
+      LeftSignal = constrain(LeftSignal, RCMin, RCMax);
+      RightSignal = constrain(RightSignal, RCMin, RCMax);
+  
+      LeftMotorSpeed = map(LeftSignal, RCMin, RCMax, MotorMaxFWD, MotorMaxREV);
+      RightMotorSpeed = map(RightSignal, RCMin, RCMax, MotorMaxFWD, MotorMaxREV);
+    } else {
+      //auto stuff
+      //get next charachter
+      Command = ' '; //set command to soemthing incase it doesn't get set later
+      if (Serial.available() > 0) { //check if a command has been received
+        NoCommand=false; //we've received soemthing, reset the flag!
+        while(Serial.available(){ //whileever there's something in the buffer
+          Command = Serial.read(); //read it. this effectively clears the buffer, ensuring the arduino keeps up
+        }
+      } else { //if nothing is availble, then nothing's been sent
+        if (NoCommand==false){  //if nocommand==false then this is the first break in recent times
+          NoCommand=true;  //set the flag
+          LastCommand=millis();  //record the time we first got a 'timeout'
+        } else {
+          if(millis()-LastCommand>200){  //if its been a while
+            LaserOn=false;                 //turn everything
+            LeftMotorSpeed = MotorNeutral; //everything
+            RightMotorSpeed = MotorNeutral; //off
+          }}
+      }
+      
+      //set outputs correctly
+      //select case
+      switch (Command) {
+        case 'F':        //F=forward
+          LaserOn=false;
+          LeftMotorSpeed = MotorMaxFWD;
+          RightMotorSpeed = MotorMaxFWD;
+        case 'B':        //B=backward
+          LaserOn=false;
+          LeftMotorSpeed = MotorMaxREV;
+          RightMotorSpeed = MotorMaxREV;
+        case 'S':        //S=stop
+          LaserOn=false;
+          LeftMotorSpeed = MotorNeutral;
+          RightMotorSpeed = MotorNeutral;
+        case 'L':        //L=rotate left
+          LaserOn=false;
+          LeftMotorSpeed = MotorMaxREV;
+          RightMotorSpeed = MotorMaxFWD;
+        case 'R':         //R=rotate right
+          LaserOn=false;
+          LeftMotorSpeed = MotorMaxFWD;
+          RightMotorSpeed = MotorMaxREV;
+        case 'Z':        //Z=fire laser
+          LaserOn=true;
+          LeftMotorSpeed = MotorNeutral;
+          RightMotorSpeed = MotorNeutral;
+        case 'z':        //z=turn laser off
+          LaserOn=false;
+          LeftMotorSpeed = MotorNeutral;
+          RightMotorSpeed = MotorNeutral;
+     }///end switch
+   }//end auto
+  } else {
+    // Enable Signal Pulse timed out or low - we have to assume RC has gone away
+    // set everything to neutral
+    LaserOn=false;
+    LeftMotorSpeed = MotorNeutral;
+    RightMotorSpeed = MotorNeutral;
+   Serial.println("**** Enable Signal lost****");
+  }
 
-// todo: check for enable channel level
-LeftMotorSignal = FWDSignal; // mixing: FWDSignal+SteeringSignal-3000; //RC signal midpoint assumed 1500, this makes motorspeed value +/-1000
-RightMotorSignal = SteeringSignal; //mixing: FWDSignal-SteeringSignal-3000;
-
-  LeftMotorSpeed = map(LeftMotorSignal, RCMin, RCMax, MotorMaxFWD, MotorMaxREV);
-  RightMotorSpeed = map(RightMotorSignal, RCMin, RCMax, MotorMaxFWD, MotorMaxREV);
-
-//debugging
+  //debugging
   analogWrite(LeftMotorOutPin,LeftMotorSpeed);
   analogWrite(RightMotorOutPin,RightMotorSpeed);
+  digitalWrite(LaserOutPin, LaserOn);
   Serial.print("left = " );                       
-  Serial.print(LeftMotorSignal);   
+  Serial.print(LeftSignal);   
   Serial.print("right = " );                       
-  Serial.println(RightMotorSignal);  
+  Serial.println(RightSignal);  
   delay(20);
 }
